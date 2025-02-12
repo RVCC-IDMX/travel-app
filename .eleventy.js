5a9b2c62b6604153d239a10378bb1e32ca93928f
SteveCaruso <steven.caruso@raritanval.edu>
2025-02-12 16:21:54 -0500

    add eleventy

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function (eleventyConfig) {
+
+    //Passthru files
+    eleventyConfig.addPassthroughCopy("./src/**/*.jpg");
+    eleventyConfig.addPassthroughCopy("./src/**/*.jpeg");
+    eleventyConfig.addPassthroughCopy("./src/**/*.png");
+    eleventyConfig.addPassthroughCopy("./src/**/*.svg");
+    eleventyConfig.addPassthroughCopy("./src/**/*.pdf");
+
+    //Ignore the output directory
+    eleventyConfig.ignores.add("_site");
+
+    //Grab all cities
+    eleventyConfig.addCollection("cities", function(collectionApi) {
+        return collectionApi.getFilteredByGlob("./src/cities/*/index.md");
+    })
+
+    return {
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dir : {
+            input: "./src",
+            output: "./_site"
+        }
+}
+
+}
