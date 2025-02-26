056f2161471b9e9b70010bec20bd22842fab6de4
Paulo Santos <zebroa10@gmail.com>
2025-02-26 16:54:16 -0500

    catch up with prototype

--- Prototype/main.js
+++ Prototype/main.js
@@ -2,3 +2,13 @@ document.querySelector(".Destinations").addEventListener("mouseover",(e) => {
     console.log("hover")
 })
 
+function myFunction() {
+    var x =
+    document.getElementById("myMenu");
+    if (x.className === "Menu") {
+        x.className === " responsive";
+    } else {
+        x.className = "Menu";
+    }
+}
+
